9899babb3f9b3653d6ee86dd28d0efe1d46e367c
Kei Sasaki <sasaki.kei@nitzlab.com>
2016-05-12 17:25:59 +0000

    新着、プロフィールのレイアウト

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@ $(function(){
     var box    = $("#menuWrap");
     var boxTop = box.offset().top;
     $(window).scroll(function () {
-        if($(window).scrollTop() >= boxTop) {
+        if($(window).scrollTop() >= boxTop - 14) {
             box.addClass("fixed");
         } else {
             box.removeClass("fixed");
