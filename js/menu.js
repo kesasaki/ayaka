e811ada972eb1828d994b4cd4da053da6b6905fe
norisu0313 <norisu0313@gmail.com>
2016-05-19 00:04:47 +0900

    menu を押した時のスクロールアニメーション

--- js/menu.js
+++ js/menu.js
@@ -8,4 +8,14 @@ $(function(){
             box.removeClass("fixed", 1000);
         }
     });
+
+    var time = 500;
+    $('a[href^=#]').click(function() {
+         var target = $(this.hash);
+         if (!target.length) return ;
+         var targetY = target.offset().top;
+         $('html,body').animate({scrollTop: targetY}, time, 'swing');
+         window.history.pushState(null, null, this.hash);
+         return false;
+    });
 });
