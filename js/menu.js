cb8e3e131a4de8f40e0e3383e7285b73dccfa879
Kei Sasaki <sasaki.kei@nitzlab.com>
2016-05-23 13:48:06 +0000

    メニューのアニメーションの変更

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,4 @@
 $(function(){
-    var box    = $("#menuWrap");
-    var boxTop = box.offset().top;
-    $(window).scroll(function () {
-        if($(window).scrollTop() >= boxTop) {
-            box.addClass("fixed", 1000);
-        } else {
-            box.removeClass("fixed", 1000);
-        }
-    });
-
     var time = 500;
     $('a[href^=#]').click(function() {
          var target = $(this.hash);
@@ -17,5 +7,21 @@ $(function(){
          $('html,body').animate({scrollTop: targetY}, time, 'swing');
          window.history.pushState(null, null, this.hash);
          return false;
+    });
+
+    var box    = $("#menuWrap");
+    var boxTop = box.offset().top;
+    var targetPos = 700;
+    $(window).on('scroll touchmove', function() {
+        box.removeClass('fixed');
+        var ScrollPos = $(window).scrollTop();
+        if (ScrollPos > targetPos) {
+            box.stop();
+            box.css('display', 'none');
+            box.delay(300).addClass('fixed').fadeIn('fast');
+        } else {
+            box.css('display', 'inline');
+            box.css('opacity', 1);
+        }
     });
 });
