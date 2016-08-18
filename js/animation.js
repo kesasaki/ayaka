b3173cf7c2875976c9123bbfbbff23054ea2e419
norisu0313 <norisu0313@gmail.com>
2016-08-18 20:41:03 +0900

    スライドショー

--- js/animation.js
+++ js/animation.js
@@ -32,3 +32,25 @@ function effect(myDom) {
         }
     }
 }
+
+function slideSwitch() {
+    var $active = $('.slideshow img.active');
+
+    if ( $active.length == 0 ) $active = $('.slideshow img:last');
+
+    var $next =  $active.next().length ? $active.next()
+        : $('.slideshow img:first');
+
+    $active.addClass('last-active');
+
+    $next.css({opacity: 0.0})
+        .addClass('active')
+        .animate({opacity: 1.0}, 1000, function() {
+            $active.removeClass('active last-active');
+        });
+}
+
+$(function() {
+    setInterval( "slideSwitch()", 5000 );
+});
+
